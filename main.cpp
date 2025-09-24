29974a4fcc1606e6b48254308010b926624ef2d2
D1ther <maxnozhkin2006@gmail.com>
2025-09-24 21:35:17 +0300

    laboratory work 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
+#include <string>
 
-// Перша лабораторна 
+int main()
+{
+    while (true)
+    {
+        double number_1;
+        double number_2;
+        std::string operation;
 
-int main() {
-    std::cout << "Максим Ножкін ІП-22" << std::endl;
-    return 0;
+        try
+        {
+            std::cout << "Введіть перше число: ";
+            std::cin >> number_1;
+
+            std::cout << "Введіть друге число: ";
+            std::cin >> number_2;
+
+            std::cout << "Оберіть операцію (+, -, *, /): ";
+            std::cin >> operation;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Некоректне введення. Спробуйте ще раз." << std::endl;
+            break;
+        }
+
+        if (operation == "+")
+        {
+            double result = number_1 + number_2;
+            std::cout << number_1 << "+" << number_2 << "=" << result << std::endl;
+        }
+        else if (operation == "-")
+        {
+            double result = number_1 - number_2;
+            std::cout << number_1 << "-" << number_2 << "=" << result << std::endl;
+        }
+        else if (operation == "*")
+        {
+            double result = number_1 * number_2;
+            std::cout << number_1 << "*" << number_2 << "=" << result << std::endl;
+        }
+        else if (operation == "/")
+        {
+            if (number_2 == 0)
+            {
+                std::cout << "Ділити на нуль - не чотко 😡" << std::endl;
+            }
+            else
+            {
+                double result = number_1 / number_2;
+                std::cout << number_1 << "/" << number_2 << "=" << result << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Такого оператора - не існує 😭 Не чотко" << std::endl;
+        }
+        
+    }
+    
 }
-
-
