4c3da10dd2c4a69cd91ccbf7bbf7cbad0ee5d642
D1ther <maxnozhkin2006@gmail.com>
2025-10-08 20:38:12 +0300

    laboratory 3

--- main.cpp
+++ main.cpp
@@ -1,96 +1,138 @@
 #include <iostream>
-#include <string>
-#include <limits>
+#include <algorithm>
 
-double add(double a, double b)
+int CheckArray(int array[], int size)
 {
-    return a + b;
-}
-
-double subtract(double a, double b)
-{
-    return a - b;
-}
-
-double multiply(double a, double b)
-{
-    return a * b;
-}
-
-double divide(double a, double b)
-{
-    if (b == 0)
+    for (int num=0; num<size; num++)
     {
-        throw std::invalid_argument("Ділити на нуль - не чотко 😡");
+        if (array[num] == 0)
+        {
+            std::cout << "Йоу не круто робити щось з тим чого нема, заповни масив челік 😎\n" << std::endl;
+            return 0;
+        }
     }
-    return a / b;
+    return 1;
+}
+
+int InputArray(int array[], int size)
+{
+    if (CheckArray(array, size))
+    {
+        std::cout << "твій масив готовий, роби шо хоч 🙄\n" << std::endl;
+        return 0;
+    }
+    std::cout << "Введіть " << size << " цілих чисел:" << std::endl;
+    for (int num=0; num<size; num++)
+    {
+        std::cin >> array[num];
+    }
+    return 0;
+}
+
+int ShowArray(int array[], int size)
+{
+    if (!CheckArray(array, size))
+    {
+        return 0;
+    }
+    std::cout << "Йоу, от що у вас в масиві 🙄: ";
+    for (int num=0; num<size; num++)
+    {
+        std::cout << array[num] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+int MaxElement(int array[], int size)
+{
+    if (!CheckArray(array, size))
+    {
+        return 0;
+    }
+    auto max_el = std::max_element(array, array + size);
+    std::cout << "Максимум 💯: " << *max_el << std::endl;
+    return 0;
+}
+
+int MinElement(int array[], int size)
+{
+    if (!CheckArray(array, size))
+    {
+        return 0;
+    }
+    auto min_el = std::min_element(array, array + size);
+    std::cout << "Мінімум 📉: " << *min_el << std::endl;
+    return 0;
+}
+
+int SortArray(int array[], int size)
+{
+    if (!CheckArray(array, size))
+    {
+        return 0;
+    }
+    std::sort(array, array + size);
+    ShowArray(array, size);
+    return 0;
+}
+
+int AverageElement(int array[], int size)
+{
+    if (!CheckArray(array, size))
+    {
+        return 0;
+    }
+    int sum = 0;
+    for (int num=0; num<size; num++)
+    {
+        sum += array[num];
+    }
+
+    std::cout << "Щось по середині +/-:\n(доречі учень знає анекдот про це)" << static_cast<double>(sum) / size << std::endl;
+    return 0;
 }
 
 int main()
 {
+    int array[10] = {0};
+    int operation;
+    std::cout << "###### 🔥 Йоу твій масив на 10 елементів готовий 🔥 ######\n\n" << std::endl;
+
     while (true)
     {
-        double number_1;
-        double number_2;
-        std::string operation;
+        std::cout << "1. Ввести цифри твого масиву 🔢\n2. Вивести ьазовий мінімум 📉\n3. Вивести шикарний максимум 💯\n4. Виведення +/- середини (учень знає анекдот про це) 😁\n5. Сортувати масив 🔀\n6. Вийти 😭" << std::endl;
 
-        std::cout << "Оберіть операцію (+, -, *, /, q): ";
+        std::cout << "Що робити: ";
         std::cin >> operation;
 
-        if (operation == "q")
+        switch (operation)
         {
-            std::cout << "Пока-пока бр бр патапім 👋" << std::endl;
-            break;
-        }
-
-        std::cout << "Введіть перше число: ";
-        if (!(std::cin >> number_1))
-        {
-            std::cout << "Не чотко введення 😭" << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            continue;
-        }
-
-        std::cout << "Введіть друге число: ";
-        if (!(std::cin >> number_2))
-        {
-            std::cout << "Ви ввели не число - ти не чотік 😡" << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            continue;
-        }
-
-        if (operation == "+")
-        {
-            double result = add(number_1, number_2);
-            std::cout << number_1 << "+" << number_2 << "=" << result << std::endl;
-        }
-        else if (operation == "-")
-        {
-            double result = subtract(number_1, number_2);
-            std::cout << number_1 << "-" << number_2 << "=" << result << std::endl;
-        }
-        else if (operation == "*")
-        {
-            double result = multiply(number_1, number_2);
-            std::cout << number_1 << "*" << number_2 << "=" << result << std::endl;
-        }
-        else if (operation == "/")
-        {
-            try
-            {
-                double result = divide(number_1, number_2);
-                std::cout << number_1 << "/" << number_2 << "=" << result << std::endl;
-            }
-            catch (const std::invalid_argument& e)
-            {
-                std::cout << e.what() << std::endl;
-            }
-        }
-        else
-        {
-            std::cout << "Такого оператора - не існує 😭 Не чотко" << std::endl;
+        
+            case 1:
+                InputArray(array, 10);
+                break;
+            
+            case 2:
+                MinElement(array, 10);
+                break;
+            case 3:
+                MaxElement(array, 10);
+                break;
+            case 4:
+                AverageElement(array, 10);
+                break;
+            case 5:
+                SortArray(array, 10);
+                break;
+            case 6:
+                std::cout << "ну пакеда 🙋‍♂️🙋‍♂️🙋‍♂️" << std::endl;
+                return 0;
+            default:
+                break;
         }
     }
+    
+
+
 }
