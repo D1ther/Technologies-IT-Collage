d18c06164ad93fa74a2387fa2263ab01f6c7bcc0
D1ther <maxnozhkin2006@gmail.com>
2025-12-03 20:46:55 +0200

    laboratory 8

--- main.cpp
+++ main.cpp
@@ -1,37 +1,86 @@
+#include <cstdlib>
 #include <iostream>
+#include <ctime>
 
-class Time
+int getRandomNumber()
 {
-    public:
-        double hours;
-        double minutes;
-        double seconds;
+    return std::rand() % 100;
+}
 
-        void addSeconds(double sec)
+void checkNumber(int number, int point)
+{
+    if (number > point)
+    {
+        std::cout << "Думаю ти маєш спуститися з неба на землю 🙄\n";
+    }
+    else if (number < point)
+    {
+        std::cout << "Підніми своє число хлопак 🔥\n";
+    }
+    else
+    {
+        std::cout << "Вітаю! Ти вгадав число! 🎉\n";
+        return;
+    }
+}
+
+void startGame()
+{
+    int lifes = 10;
+    int point = getRandomNumber();
+    int number;
+
+    std::cout << "===Вгадай число від 0 до 99. У тебе є " << lifes << " спроб===\n\n";
+    std::cin >> number;
+
+    while (true)
+    {
+        while (number != point && lifes > 1)
         {
-            seconds += sec;
-            minutes += static_cast<int>(seconds / 60);
-            hours += static_cast<int>(minutes / 60);
-            seconds = static_cast<int>(seconds) % 60;
-            minutes = static_cast<int>(minutes) % 60;
+            checkNumber(number, point);
+            --lifes;
+            std::cout << "Залишилось спроб: " << lifes << "\nСпробуй ще раз: ";
+            std::cin >> number;
+
+            if (lifes == 1)
+            {
+                std::cout << "\n===БЛІІІНН, ти програв. Загадане число було: " << point << "===\n";
+                return;
+            }
         }
 
-        void showTime()
-        {
-            std::cout << "Час: " << hours << " годин, " << minutes << " хвилин, " << seconds << " секунд." << std::endl;
-        }
-};
+        checkNumber(number, point);
+        break;
+    }
+}
+
 
 int main()
 {
-    Time time_1{1, 30, 45};
+    std::srand(std::time(0));
 
+    while (true)
+    {
+        startGame();
 
-    time_1.showTime();
+        char choice;
+        std::cout << "Бажаєш зіграти ще раз? (y/n): \n\n";
+        std::cin >> choice;
 
-
-    time_1.addSeconds(5000);
-    time_1.showTime();
-
-    return 0;
+        switch (choice)
+        {
+        case 'y':
+            startGame();
+            break;
+        
+        case 'n':
+            std::cout << "Дякую за гру! До зустрічі!\n";
+            return 0;
+        
+        default:
+            std::cout << "Невірний вибір. Спробуй ще раз.\n";
+            break;
+        }
+    }
+    
 }
